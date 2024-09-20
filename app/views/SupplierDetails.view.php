a57c8f0f6eca588907614a5fe09c4ad8e0232bfc
RSCooray <rumaincooray02@gmail.com>
2024-09-20 12:32:43 +0530

    Supplier details page

---
+++ app/views/SupplierDetails.view.php
@@ -0,0 +1,9 @@
+<div class="container-column">
+    <?php App\View::render("components/Navbar") ?>
+    <div class="container-row">
+        <?php App\View::render("components/Sidebar") ?>
+        <div class="content">
+
+        </div>
+    </div>
+</div>
