c640e4e45c56f1fc1004358f28fe1cdd5ecf4ca8
Pamudu Sarasith <pamudu.lelkada@gmail.com>
2024-11-04 13:21:11 +0530

    Enable debug mode and improve autoloading error handling in index.php

--- public/index.php
+++ public/index.php
@@ -1,29 +1,36 @@
 <?php
 
+define('DEBUG', true);
 define('ROOT_PATH', realpath(__DIR__ . "/.."));
 define('APP_PATH', ROOT_PATH . "/app");
 
 spl_autoload_register(function ($class) {
     $parts = explode('\\', $class);
 
-    if (count($parts) == 0 || $parts[0] != 'App')
+    if (empty($parts) || $parts[0] != 'App') {
         return;
+    }
 
     $path = array_slice($parts, 1, -1);
     $className = $parts[array_key_last($parts)];
 
     $includePath = APP_PATH . "/";
-    if (count($path) != 0)
+    if (count($path) != 0) {
         $includePath = $includePath . strtolower(implode('/', $path)) . "/";
+    }
     $includePath = $includePath . $className . ".php";
 
-    if (is_file($includePath))
+    if (is_file($includePath)) {
         include_once $includePath;
+    }
 });
 
 set_exception_handler(function ($e) {
     App\View::render("errors/500");
     error_log($e->getMessage());
+    if (DEBUG) {
+        echo $e->getMessage();
+    }
 });
 
 session_start();
